9cb8c5187381adf1fb4b8deb83ac38fbc96a26f2
Piyush Patel <piyushpatel.cloud@yahoo.com>
2022-03-25 20:51:59 -0400

    parsing complete. need to dockerize, csr, push to db

--- server/parsingXML.js
+++ server/parsingXML.js
@@ -2,29 +2,43 @@ const xml2js = require('xml2js')
 const axios = require('axios')
 
 const parsingXML = async (req, res) => {
-    let makes;
-    let types;
-
-    const axiosAllMakesReq = await axios("https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=XML");
-    const allMakesData = axiosAllMakesReq.data;
-
-    const axiosVehicleTypeReq = await axios("https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMakeId/440?format=xml");
-    const vehicleTypeData = axiosVehicleTypeReq.data;
-
     const xmlParser = new xml2js.Parser({
         explicitArray: false
     });
-    xmlParser.parseString(vehicleTypeData, function (err, result) {
-        // console.log(result.Response.Results.VehicleTypesForMakeIds, 'JSON')
-        makes = result.Response.Results.VehicleTypesForMakeIds;
+
+    const axiosAllMakesReq = await axios("https://vpic.nhtsa.dot.gov/api/vehicles/getallmakes?format=XML");
+    let allMakesData = [];
+
+    //! PARSING ALL VEHICLE MAKES 
+    xmlParser.parseString(axiosAllMakesReq.data, function (err, result) {
+        allMakesData = result.Response.Results.AllVehicleMakes;
     })
 
-    xmlParser.parseString(allMakesData, function (err, result) {
-        // console.log(result.Response.Results.AllVehicleMakes, 'XML')
-        types = result.Response.Results.AllVehicleMakes;
-    })
+    let allVehicles = [];
 
-    res.status(200).json({ status: 200, data: makes, types })
+    for (const make of allMakesData) {
+        const makeId = make.Make_ID;
+
+        const vehicleTypeReqXML = await axios(`https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMakeId/${makeId}?format=xml`)
+            .then(response => { return response.data })
+            .catch(e => console.log(e));
+
+        let vehicleTypeReq = [];
+
+        //! PARSING ALL VEHICLE MAKES 
+        xmlParser.parseString(vehicleTypeReqXML, function (err, result) {
+            vehicleTypeReq = result.Response.Results.VehicleTypesForMakeIds;
+        })
+
+        console.log(vehicleTypeReq, 'vehcile type req')
+
+        allVehicles.push({
+            makeId: makeId,
+            makeName: make.Make_Name,
+            vehicleTypes: vehicleTypeReq
+        })
+    }
+    res.status(200).json(allVehicles)
 };
 
 module.exports = parsingXML;
